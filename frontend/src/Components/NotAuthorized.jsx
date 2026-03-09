f49059358c22d8aa666031e52442ed7495986baa
Tristan Miguel Garcia <tristanmiguelgarcia29@gmail.com>
2026-03-09 13:33:03 -0400

    added outputs in the not authorized and page not found

--- frontend/src/Components/NotAuthorized.jsx
+++ frontend/src/Components/NotAuthorized.jsx
@@ -2,4 +2,13 @@
 // any naughty users trying to bypass the login,
 // or non-admins trying to reach admin pages should be brought here
 
-export default function NotAuthorized() {}
+import { Link } from "react-router-dom";
+
+export default function NotAuthorized() {
+  return (
+    <div>
+      <h1>Error 403: User Not Authorized to see this page</h1>
+      <Link to="/">Please login first</Link>
+    </div>
+  );
+}
--- frontend/src/Components/PageNotFound.jsx
+++ frontend/src/Components/PageNotFound.jsx
@@ -1,4 +1,13 @@
 // PAGE NOT FOUND
 // Any random crap put in as a url should lead here
 
-export default function PageNotFound() {}
+import { Link } from "react-router-dom";
+
+export default function NotAuthorized() {
+  return (
+    <div>
+      <h1>Error 403: User Not Authorized to see this page</h1>
+      <Link to="/">Please login first</Link>
+    </div>
+  );
+}
