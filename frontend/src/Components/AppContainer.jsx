7f70a925dac5946b4a28f8501025c279d6fdcc56
Damien Sauve <damien.sauve4@gmail.com>
2026-03-26 13:16:28 -0400

    added navbar routes, and several components that will be used in the navbar

--- frontend/src/Components/AppContainer.jsx
+++ frontend/src/Components/AppContainer.jsx
@@ -1,6 +1,7 @@
 // The big component, all (or most of) the logic should live here,
 // and all (or most of) the routes should originate from here (other than the ones in app.jsx)
 import NavBar from "./NavBar";
+// TODO: currentUser set to admin by default for debugging purposes -- needs to be changed eventually
 export default function AppContainer({ currentUser = "admin" }) {
   return (
     <>
