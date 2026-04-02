0536d936dfba6704cdb38962ca89f23a58819d4c
Chad Watson <chadwatson2005@gmail.com>
2026-04-01 20:00:25 -0400

    added player model... thats it

--- models/player.js
+++ models/player.js
@@ -1,1 +1,51 @@
 // This is for later once login is done and we are inside the main page
+import mongoose from "mongoose";
+
+const playerSchema = new mongoose.Schema ({
+
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    jerseyNumber: {
+      type: Number,
+      required: true,
+    },
+
+    //height, weight, date of birth????
+
+    position: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    image: {
+      type: String,
+      default: "",
+    },
+
+    team: {
+      type: String,
+      default: "",
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Player = mongoose.model("Player", playerSchema);
+
+export default Player;
