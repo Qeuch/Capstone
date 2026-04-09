2e94fc1b0c5cbb22d23b5fd71c45268fd96c65db
Damien Sauve <damien.sauve4@gmail.com>
2026-04-09 15:10:01 -0400

    fixed big whoopsie

---
+++ frontend/src/Components/PlayerCard.jsx
@@ -0,0 +1,20 @@
+export default function PlayerCard({
+  // Add whatever's necessary
+  first_name,
+  last_name,
+  player_picture,
+  position,
+  jersey_number,
+}) {
+  return (
+    <>
+      <div>
+        <img src="" alt="" />
+        <p>Player Name</p>
+        <p>position</p>
+        <p>jersey num</p>
+        <button>View Stats</button>
+      </div>
+    </>
+  );
+}
